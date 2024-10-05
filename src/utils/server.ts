6d3a4c4ef258684cdd5eef9eeb4e637b62fe14ba
Gaurav Singh <gauravnumber@gmail.com>
2024-10-05 18:40:55 +0530

    url

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -3,16 +3,18 @@
 import prisma from "@/db/prisma";
 import { uuid } from "@/libs/uuid";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createUrlShortner = async (formData: FormData) => {
   const url = formData.get('url') as string
-  
+  const slug =uuid(8)
   await prisma.url_lists.create({
     data: {
       url,
-      slug: uuid(8),
+      slug,
     },
   });
 
   revalidatePath("/")
+  redirect(`/congrate?slug=${slug}`)
 };
