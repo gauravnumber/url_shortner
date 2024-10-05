e264acfc130483f08118e9c6e4843d0dd5260a9a
Gaurav Singh <gauravnumber@gmail.com>
2024-10-05 20:18:50 +0530

    subtle

--- src/app/congrate/page.tsx
+++ src/app/congrate/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const Congrate = () => {
   const searchParams = useSearchParams();
   const slug = searchParams.get("slug");
   // const copiedSite = `${window.location.protocol}//${window.location.hostname}/${slug}`;
   return (
-    <>
+    <Suspense fallback={<p>loading...</p>}>
       <p>Congratulations you created link.</p>
       <p>Copy this link:</p>
       <p>Slug: {slug}</p>
       {/* <p>{copiedSite}</p> */}
-    </>
+    </Suspense>
   );
 };
 
