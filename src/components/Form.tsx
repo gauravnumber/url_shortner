cd3b251ce9ebdc7256bd2b0dc60221cda7d73f4c
Gaurav Singh <gauravnumber@gmail.com>
2024-10-07 12:07:35 +0530

    frontend custom slug

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,24 @@
 import { createUrlShortner } from "@/utils/server";
+import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 const Form = () => {
   return (
     <div className="mx-auto max-w-sm pt-1">
-      <form action={createUrlShortner} className="flex flex-col gap-2">
-        {/* <input
-          type="url"
-          name="url"
-          className="rounded-md border-black "
-        /> */}
-        <Input type="url" name="url" autoFocus/>
-        <input
-          type="submit"
-          value="create shortner"
-          className="rounded-full bg-cyan-400 py-2 px-1"
-        />
+      <form
+        action={createUrlShortner}
+        className="flex flex-col gap-2 w-full max-w-sm "
+      >
+        <Label htmlFor="url">URL</Label>
+        <Input type="url" name="url" autoFocus  placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"/>
+
+        <Label htmlFor="custom-slug">Custom Slug (optional)</Label>
+        <Input type="text" name="custom-slug" placeholder="e.g. cat, dog, etc." />
+
+        <Button type="submit" className="rounded-full">
+          Create
+        </Button>
       </form>
     </div>
   );
